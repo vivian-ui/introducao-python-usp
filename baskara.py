7d97329b2ca45a8323ba99d9d02518a587849d16
vivian-ui <vi_cunha@hotmail.com>
2021-12-15 15:16:48 -0300

    nosvos arquivos

---
+++ baskara.py
@@ -0,0 +1,20 @@
+import math
+
+a = float(input("Digite o valor de a: "))
+b = float(input("Digite o valor de b: "))
+c = float(input("Digite o valor de c: "))
+
+delta = b ** 2 - 4 * a * c
+
+if delta == 0:
+    raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+    print("A única raíz é: ", raiz1)
+else:
+    if delta < 0:
+        print("Essa equação não possui raizes reais")
+    else:
+        raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+        raiz2 = (-b - math.sqrt(delta)) / (2 * a)
+        print("Primeira raíz é: ", raiz1)
+        print("Segunda raíz é: ", raiz2)
+
